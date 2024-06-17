a27e6f389759a259d446fb205868b279d40d807b
Brian Howell <brianhowell@pm.me>
2024-06-17 16:59:04 -0400

    create-tables migration

--- data/migrations/20240617204828_create-tables.js
+++ data/migrations/20240617204828_create-tables.js
@@ -1,11 +1,59 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.up = function (knex) {};
+exports.up = function (knex) {
+  // Tables
+  // - recipes
+  // - steps
+  // - ingredients
+  // - recipe_ingredients
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.down = function (knex) {};
+  return knex.schema
+    .createTable("recipes", (tbl) => {
+      tbl.increments("recipe_id");
+      tbl.string("recipe_name", 128).notNullable();
+      tbl.timestamp("created_at").defaultTo(knex.fn.now());
+    })
+    .createTable("steps", (tbl) => {
+      tbl.increments("step_id");
+      tbl.integer("step_number").notNullable();
+      tbl.string("step_instructions").notNullable();
+      tbl
+        .integer("recipe_id")
+        .unsigned()
+        .notNullable()
+        .references("recipe_id")
+        .inTable("recipes")
+        .onDelete("RESTRICT")
+        .onUpdate("RESTRICT");
+    })
+    .createTable("ingredients", (tbl) => {
+      tbl.increments("ingredient_id");
+      tbl.string("ingredient_name", 128).notNullable();
+    })
+    .createTable("recipe_ingredients", (tbl) => {
+      tbl
+        .integer("recipe_id")
+        .unsigned()
+        .notNullable()
+        .references("recipe_id")
+        .inTable("recipes")
+        .onDelete("RESTRICT")
+        .onUpdate("RESTRICT");
+      tbl
+        .integer("ingredient_id")
+        .unsigned()
+        .notNullable()
+        .references("ingredient_id")
+        .inTable("ingredients")
+        .onDelete("RESTRICT")
+        .onUpdate("RESTRICT");
+      tbl.float("quantity").notNullable();
+      tbl.primary(["recipe_id", "ingredient_id"]);
+    });
+};
+
+exports.down = function (knex) {
+  return knex.schema
+    .dropTableIfExists("recipe_ingredients")
+    .dropTableIfExists("ingredients")
+    .dropTableIfExists("steps")
+    .dropTableIfExists("recipes");
+};
