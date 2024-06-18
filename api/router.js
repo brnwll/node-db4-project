8e9c7043e3196d839816c20b8ae21d31cdf38d27
Brian Howell <brianhowell@pm.me>
2024-06-18 15:39:32 -0400

    Remove comment

--- api/router.js
+++ api/router.js
@@ -1,11 +1,3 @@
-/*
-
-#### Endpoint
-
-Write an endpoint to fetch a recipe by its id, using the `getRecipeById(recipe_id)` function.
-
-*/
-
 const express = require("express");
 const helpers = require("./model");
 
